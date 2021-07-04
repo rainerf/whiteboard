099cf23f4cc1e48e4b97e456cd626463f74a47a8
rainer <rainer@findenig.net>
2021-07-04 12:03:18 +0200

    clean up the drawing code

--- tools/pentool.cpp
+++ tools/pentool.cpp
@@ -11,29 +11,25 @@ void PenTool::setPen(int thickness) {
 void PenTool::handleTabletPress(QGraphicsView &view, QTabletEvent &event) {
     m_group = new QGraphicsItemGroup();
     m_group->setFlags(QGraphicsItem::ItemIsMovable | QGraphicsItem::ItemIsSelectable);
-
-    m_path = new QGraphicsPathItem();
-    m_previous = view.mapToScene(event.pos());
-    QPainterPath p;
-    p.moveTo(m_previous);
-    m_path->setPath(p);
-    m_group->addToGroup(m_path);
     view.scene()->addItem(m_group);
+
+    m_previous = view.mapToScene(event.pos());
 }
 
 void PenTool::handleTabletMove(QGraphicsView &view, QTabletEvent &event) {
-    if (m_path) {
-        QPainterPath path = m_path->path();
-        m_previous = view.mapToScene(event.pos());
-        path.lineTo(m_previous);
-        m_path->setPath(path);
+    if (m_group) {
+        auto current = view.mapToScene(event.pos());
+
+        QPainterPath p;
+        p.moveTo(m_previous);
+        p.lineTo(current);
 
         m_path = new QGraphicsPathItem();
         m_path->setPen(QPen(m_color, event.pressure() * m_thickness, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin));
-        QPainterPath p;
-        p.moveTo(m_previous);
         m_path->setPath(p);
         m_group->addToGroup(m_path);
+
+        m_previous = current;
     }
 }
 
